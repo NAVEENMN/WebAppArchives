03755248111ea8dfaad9a8ea4d1c5bbd49794f6c
naveenmysore <navimn1991@gmail.com>
2020-01-17 14:48:27 -0800

    add and delete

--- src/db.py
+++ src/db.py
@@ -1,11 +1,66 @@
+import json
 import pymongo
 
 class database():
     def __init__(self):
-        conn = "mongodb+srv://test_user:test@cluster0-onaoj.mongodb.net/test?retryWrites=true&w=majority"
-        self.client = pymongo.MongoClient(conn)
-        self.db = client.test
+        db_url = "mongodb+srv://test_user:test@cluster0-onaoj.mongodb.net/"
+        db_url = db_url + "test?retryWrites=true&w=majority"
+        self.client = pymongo.MongoClient(db_url)
+        self.dbs = self.client.list_database_names()
 
-    def add_entry(self, data):
-        post_id = db.posts.insert_one(data).inserted_id
-        return post_id
+    def add_entry(self, db_name, coll_name, data):
+        resp = dict()
+        resp['success'], resp['payload'] = False, None
+        if (db_name in self.dbs) and (coll_name in self.client[db_name].list_collection_names()):
+            ref = self.client[db_name][coll_name]
+            try:
+                post_id = ref.insert_one(data).inserted_id
+                resp['success'] = True
+                resp['payload'] = dict({'_id': None})
+                print("inserted", post_id)
+            except:
+                resp['success'] = False
+                print("insert failed")
+        else:
+            resp['success'] = False
+            resp['payload'] = json.dumps({'error':' Invalid collection'})
+            print("invalid db or collection")     
+        return json.dumps(resp)
+
+    def delete_entry(self, db_name, coll_name, query, delete_many=False):
+        resp = dict()
+        resp['success'], resp['payload'] = False, None
+        if (db_name in self.dbs) and (coll_name in self.client[db_name].list_collection_names()):
+            ref = self.client[db_name][coll_name]
+            try:
+                if delete_many:
+                    post_id = ref.delete_many(query)
+                else:
+                    post_id = ref.delete_one(query)
+                resp['success'] = True
+                resp['payload'] = dict({'_id': None})
+                print("deleted", post_id)
+            except:
+                resp['success'] = False
+                print("delete failed")
+        else:
+            resp['success'] = False
+            resp['payload'] = json.dumps({'error':' Invalid collection'})
+            print("invalid db or collection")     
+        return json.dumps(resp)
+
+    def get_collections(self, coll_name):
+        print(self.collections)
+        if coll_name in self.collections:
+            print(True)
+        print(self.client[coll_name])
+
+def main():
+    db = database()
+    #db.get_collections('Users')
+    data = json.dumps({'name':'naveen'})
+    #db.add_entry('Users', 'account', json.loads(data))
+    db.delete_entry('Users', 'account', query={'name': 'naveen'}, delete_many=True)
+
+if __name__ == "__main__":
+    main()
