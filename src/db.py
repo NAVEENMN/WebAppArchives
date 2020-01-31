7f5759e464833924d8b7d41fbc0880da83ba330d
naveenmysore <navimn1991@gmail.com>
2020-01-31 13:04:48 -0800

    logging

--- src/db.py
+++ src/db.py
@@ -1,7 +1,22 @@
 import json
+import logging
 import pymongo
 from bson import json_util
 from monogo_schema import *
+
+
+def set_logging():
+    logger = logging.getLogger('db_logger')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    ch = logging.StreamHandler()
+    logger.setLevel(logging.INFO)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
+
+
+log = set_logging()
+
 
 class database():
     def __init__(self):
@@ -13,6 +28,7 @@ class database():
         self.dbs = self.client.list_database_names()
 
     def add_entry(self, db_name, coll_name, data):
+        log.info('Adding Entry: db { %s }, collection { %s }', db_name, coll_name)
         resp = dict()
         resp['success'], resp['payload'] = False, None
         if (db_name in self.dbs) and (coll_name in self.client[db_name].list_collection_names()):
@@ -21,17 +37,19 @@ class database():
                 post_id = ref.insert_one(data).inserted_id
                 resp['success'] = True
                 resp['payload'] = {'_id': None}
-                print("inserted", post_id)
+                log.info('Adding entry success')
             except:
                 resp['success'] = False
-                print("insert failed")
+                log.error('Adding entry failed: Operation failed')
         else:
             resp['success'] = False
-            resp['payload'] = {'error':' Invalid collection'}
-            print("invalid db or collection")     
+            resp['payload'] = {'error': ' Invalid collection'}
+            log.error('Adding entry failed: Invalid collection')
         return resp
 
     def delete_entry(self, db_name, coll_name, query, delete_many=False):
+        log.info('Delete Entry: db { %s }, collection { %s }', db_name, coll_name)
+        log.info('Query %s', query)
         resp = dict()
         resp['success'], resp['payload'] = False, None
         if (db_name in self.dbs) and (coll_name in self.client[db_name].list_collection_names()):
@@ -46,14 +64,16 @@ class database():
                 print("deleted", post_id)
             except:
                 resp['success'] = False
-                print("delete failed")
+                log.error('Delete entry failed: Operation failed')
         else:
             resp['success'] = False
-            resp['payload'] = json.dumps({'error':' Invalid collection'})
-            print("invalid db or collection")     
+            resp['payload'] = json.dumps({'error': 'Invalid collection'})
+            log.error('Delete entry failed: Invalid collection')
         return json.dumps(resp)
 
     def get_entry(self, db_name, coll_name, query=None):
+        log.info('Find Entry: db { %s }, collection { %s }', db_name, coll_name)
+        log.info('Query %s', query)
         resp = dict()
         resp['success'] = False
         if (db_name in self.dbs) and (coll_name in self.client[db_name].list_collection_names()):
@@ -65,10 +85,11 @@ class database():
                 resp['success'] = True
             except:
                 resp['success'] = False
-                print("find failed")
+                log.error('Find entry failed: Operation failed')
         else:
             resp['success'] = False
-            resp['payload'] = {'error':' Invalid collection'}
+            resp['payload'] = {'error': ' Invalid collection'}
+            log.error('Get entry failed: Invalid collection')
         return resp
 
     def find_users(self, db_name, coll_name, query, all_entry=False):
@@ -90,7 +111,7 @@ class database():
                 print("find failed")
         else:
             resp['success'] = False
-            resp['payload'] = {'error':' Invalid collection'}
+            resp['payload'] = {'error': ' Invalid collection'}
             print("invalid db or collection")     
         return resp
 
@@ -103,8 +124,8 @@ class database():
 def main():
     db = database()
     #db.get_collections('Users')
-    #data = json.dumps({'name':'naveen'})
-    #db.add_entry('Users', 'account', json.loads(data))
+    data = json.dumps({'name':'naveen'})
+    db.add_entry('Users', 'account', json.loads(data))
     #db.delete_entry('Users', 'account', query={'name': 'naveen'}, delete_many=True)
     #db.find_entry('Medteam', 'Accounts', query={'specialities': ['Ayurveda'] }, all_entry=True)
     #r = db.find_entry('Medteam', 'Accounts', query={'user_id':'91235'}, all_entry=False)
